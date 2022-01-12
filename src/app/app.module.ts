6866c159b64037765b631cbbf65d00e9657cbb86
Shakil Ahammed <shakil@nascenia.com>
2022-01-12 23:10:01 +0600

    welcome and map components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    WelcomeComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
